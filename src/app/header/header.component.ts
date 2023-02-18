6ffc74e31f18694af01ed5815cfc76d14a343f03
Ouattara Katina <ouattarapevirgile@gmail.com>
2023-02-18 21:40:33 +0000

    finish header and respnsive design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @HostListener('window:scroll', ['$event']) onScroll(){
+    if(window.scrollY>100){
+      this.navBarFixed = true;
+    }else{
+      this.navBarFixed = false
+    }
+  }
 
-  public nbrCmd!: number
+  public nbrCmd!: number;
+
+  iconBtn!: string;
+  menuVariable: boolean =false;
+
+  navBarFixed: boolean = false;
+
+  openMenu(){
+    if(this.menuVariable == true){
+      this.menuVariable = false;
+      this.iconBtn = "menu";
+    }else{
+      this.menuVariable = true;
+      this.iconBtn = "close";
+    }
+  }
+
+  
   constructor() { }
 
   ngOnInit(): void {
     this.nbrCmd = 1;
+    this.iconBtn = "menu"
   }
+ 
 
 }
