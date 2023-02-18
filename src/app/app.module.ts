d188886bd2183b83e00a6595f86b85622766e57d
Ouattara Katina <ouattarapevirgile@gmail.com>
2023-02-18 17:06:22 +0000

    fix header for desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
