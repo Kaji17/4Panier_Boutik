dc5ef4a7858745d21f754acf401a98de8ad995b5
Ouattara Katina <ouattarapevirgile@gmail.com>
2023-02-19 08:20:45 +0000

    création du app-routing et du modules pour le component chaussures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ChaussuresListComponent } from './chaussures/chaussures-list/chaussures-list.component';
+import { ChaussuresDetailComponent } from './chaussures/chaussures-detail/chaussures-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccueilComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ChaussuresListComponent,
+    ChaussuresDetailComponent
   ],
   imports: [
     BrowserModule
