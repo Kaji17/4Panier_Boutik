0b3980117747130dc58134f25e11a8f30ca85246
Ouattara Katina <ouattarapevirgile@gmail.com>
2023-02-19 08:22:59 +0000

    ajout du component chaussures et des images de logo des marques

---
+++ src/app/chaussures/chaussures-detail/chaussures-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chaussures-detail',
+  templateUrl: './chaussures-detail.component.html',
+  styleUrls: ['./chaussures-detail.component.scss']
+})
+export class ChaussuresDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
