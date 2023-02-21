3d9e52b8a1f179c70dfbcb3d62be3d08a434b298
Ouattara Katina <ouattarapevirgile@gmail.com>
2023-02-21 15:38:34 +0000

    panier finish controller

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -7,16 +7,31 @@ import { PanierService } from '../shared/services/panier.service';
   styleUrls: ['./panier.component.scss']
 })
 export class PanierComponent implements OnInit {
-  panier!: boolean
-  panierEmpty!: boolean
 
   product: any = [];
   grandTotal: number =0
   constructor(private cartservice: PanierService) { }
 
   ngOnInit(): void {
-    this.panierEmpty = false
-    this.panier = true
+    this.actuailise()
+  }
+
+  removeArticle(item:any):void{
+    this.cartservice.removeFromCart(item)
+    this.actuailise()
+  }
+
+  decrement(item:any):void{
+    this.cartservice.decrementFromCart(item)
+    this.actuailise()
+  }
+
+  public addToCart(item: any): void{
+    this.cartservice.incrementFromCart(item)
+    this.actuailise()
+  }
+
+  public actuailise():void{
     this.product = this.cartservice.getProducts()
     this.grandTotal = this.cartservice.getTotalAmount()
   }
